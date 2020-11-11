7823af92f61f3a73b2b796004fb6011d750ef8e3
RohitRajP <RohitRajP>
2020-11-11 10:25:21 +0530

    Completed Local server checks

--- start.py
+++ start.py
@@ -2,11 +2,15 @@ import subprocess
 from colorit import color, init_colorit, Colors
 import os
 import wget
+from pathlib import Path
+import requests
+import time
+
 
 # COMMON--------------------------------------------------------------------------------------------
 
 
-def print_message(message, type, prefix):  # used to print messages to console
+def print_message(message, type, prefix, end="\n"):  # used to print messages to console
     print(prefix, end=" ")
     if (type == "success"):
         print(color("√", Colors.green), end=" ")
@@ -14,14 +18,14 @@ def print_message(message, type, prefix):  # used to print messages to console
         print(color("X", Colors.red), end=" ")
     elif (type == "info"):
         print(color("!", Colors.blue), end=" ")
-    print(message)
+    print(message, end=end)
 
 
 # MONGODB INSTALLATION--------------------------------------------------------------------------------------------
 
 def check_mongodb():  # used to check if mongodb has been installed
     mongoCheck = subprocess.Popen(
-        "mongodd --version", shell=True, stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
+        "mongod --version", shell=True, stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
     mongoCheck_status = mongoCheck.wait()
     return (mongoCheck_status == 0)
 
@@ -62,9 +66,80 @@ def initiateMongoDbChecks():  # used to initiate and handle mongodb installation
             invoke_downloaded_mongodbinstaller()
 
 
+# SERVER INSTALLATION--------------------------------------------------------------------------------------------
+
+# custom download progress bar for wget
+def bar_custom_local_server(current, total, width=80):
+    if current == total:
+        print_message(
+            "Downloading latest server version: %d%%" % (
+                current / total * 100), "info", "  -", "\n")
+    else:
+        print_message(
+            "Downloading latest server version: %d%%" % (
+                current / total * 100), "info", "  -", "\r")
+
+
+def delete_server_instance(pathStr):  # used to delete the local copy of server
+    print_message("Deleting local server instance", "info", "  -")
+    subprocess.Popen(
+        "del sellerspotserver.exe", shell=True, stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
+
+
+def stop_server():  # used to stop execution of any running server instances
+    print_message("Stopping running server instance", "info", "  -")
+    subprocess.Popen(
+        "taskkill /f /im sellerspotserver.exe", shell=True, stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
+
+
+def get_latest_server(pathStr):  # used to get the latest version of local server
+    wget.download(
+        'https://sellerspotdev.s3.ap-south-1.amazonaws.com/sellerspotserver.exe', pathStr, bar=bar_custom_local_server)
+
+
+def initiate_server(pathStr):  # used to initiate the latest downloaded server
+    print_message(
+        "Initialising local server instance...", "info", "  -")
+    subprocess.Popen(
+        "sellerspotserver.exe", shell=True, stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
+
+
+def check_local_server_status():  # used to check the status of local server
+    try:
+        response = requests.head('http://localhost:40040')
+        if response.status_code == 200:
+            print_message(
+                "Local server up and running", "success", "  -")
+        else:
+            print_message(
+                "Local server initialisation failed - please contact developers", "failure", "  -")
+    except requests.exceptions.ConnectionError as e:
+        print_message(
+            "Local server initialisation failed - please contact developers", "failure", "  -")
+
+
+def initiateLocalServerChecks():  # used to initiate and handle installation of local server
+    print_message("Local server check", "success", "")
+    # path to store the local server in
+    pathStr = str(Path.home()) + \
+        "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+    os.chdir(pathStr)
+    # stopping running servers
+    stop_server()
+    time.sleep(1)
+    # deleting local server instance
+    delete_server_instance(pathStr)
+    # dowloading latest version of localconnect server
+    get_latest_server(pathStr)
+    # initializing local server instance
+    initiate_server(pathStr)
+    # waiting for server to initialize
+    time.sleep(5)
+    # check if local server is up and running
+    check_local_server_status()
+
+
 # MAIN FUNCTION--------------------------------------------------------------------------------------------
-
-
 if __name__ == "__main__":
 
     # clearing output and initializing text color engine
@@ -72,3 +147,6 @@ if __name__ == "__main__":
 
     # initialising mongoDb checks
     initiateMongoDbChecks()
+
+    # initialising localServer checks
+    initiateLocalServerChecks()
